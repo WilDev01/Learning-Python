5d25f67630ad26558c711328dcfe769b9c07dd09
wilmer3004 <wilmercaperahernandez@gmail.com>
2024-11-11 19:38:03 -0500

    Update str_type_functions.py

--- basics_concept/data_types/str_type_functions.py
+++ basics_concept/data_types/str_type_functions.py
@@ -55,7 +55,49 @@ print(data_text_upper)
 
 # Capitalize first letter
 # Capitalizar primera letra
-
+# The capitalize method capitalize the first letter of the text
+# El metodo capitaze capitaliza la primera letra del texto
 data_text_capitalize_first_letter = data_text_lower.capitalize()
 print(data_text_capitalize_first_letter)
 
+# Capitalize every word
+# Capitalizar cada palabra
+# The Python title() function is used to change the initial character in each word to Uppercase and the subsequent characters to Lowercase and then returns a new string.
+# La función title() de Python se utiliza para cambiar el carácter inicial de cada palabra a mayúsculas y los caracteres subsiguientes a minúsculas y luego devuelve una nueva cadena.
+data_text_title = data_text.title()
+print(data_text_title)
+
+# Swapcase
+# Caso de intercambio
+# The swapcase() function is a built-in method in Python that can be used with the string data type. This function allows you to convert uppercase letters to lowercase and vice versa within a string.
+# La función swapcase() es un método integrado en Python que se puede utilizar con el tipo de datos string. Esta función permite convertir letras mayúsculas en minúsculas y viceversa dentro de una cadena.
+data_text_swapcase = data_text_title.swapcase()
+print(data_text_swapcase)
+
+# splitting and joining chains
+# división y unión de cadenas
+
+# Split
+# Dividir
+# The split() function still works if the separator is not specified by considering white spaces, as the separator to separate the given string or given line.
+# La función split() aún funciona si no se especifica el separador considerando los espacios en blanco como el separador para separar la cadena o la línea dada.
+data_text_split = data_text.split()
+print(data_text_split)
+
+# Dividir por lineas
+# splitlines
+# splitlines() is a built-in string method in Python that is used to split a multi-line string into a list of lines. It recognizes different newline characters such as \n , \r , or \r\n and splits the string at those points.
+# splitlines() es un método de cadena integrado en Python que se utiliza para dividir una cadena de varias líneas en una lista de líneas. Reconoce diferentes caracteres de nueva línea, como \n , \r o \r\n, y divide la cadena en esos puntos.
+
+data_text_splitline = data_text.splitlines()
+print(data_text_splitline)
+
+# Join
+# Unir
+# Join in Python is a built-in method used to join the elements of an iterable, separated by a string separator that you specify. So, whenever you want to join the elements of an iterable and convert it into a string, you can use string join in Python.
+# Join en Python es un método integrado que se utiliza para unir los elementos de un iterable, separados por un separador de cadena que usted especifica. Por lo tanto, siempre que desee unir los elementos de un iterable y convertirlo en una cadena, puede utilizar la unión de cadenas en Python.
+# Lista de elementos que quieres unir en una cadena
+data_text_list = ["Hello", "world", "Python", "is", "great"]
+
+data_text_join = " ".join(data_text_list)
+print(data_text_join) 
