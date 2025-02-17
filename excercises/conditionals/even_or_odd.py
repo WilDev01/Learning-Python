e303920c63acfe5726a8fc34aaa958eb4ca9e470
WilDev01 <wilmercaperahernandez@gmail.com>
2025-02-17 09:37:39 -0500

    Create even_or_odd.py

---
+++ excercises/conditionals/even_or_odd.py
@@ -0,0 +1,12 @@
+# This program performs the function of identifying whether the number is even or odd.
+
+print("Number is even or odd")
+num = float(input("Enter a number: "))
+
+# This conditional identity identifies whether it is an even or odd number.
+if num > 0:
+    print("Positive.")
+elif num < 0:
+    print("Negative.")
+else:
+    print("Number is 0.")
